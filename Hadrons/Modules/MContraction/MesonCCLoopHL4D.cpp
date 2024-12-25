22a214dc1e6852b9f44a717c37332da9fec3e10b
Vaishakhi <vaishu.moningi@gmail.com>
2024-12-25 12:11:36 -0500

    HL with one inversion

---
+++ Hadrons/Modules/MContraction/MesonCCLoopHL4D.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MContraction/MesonCCLoopHL4D.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MContraction;
+
+template class Grid::Hadrons::MContraction::TStagMesonLoopCCHL4D<STAGIMPL,STAGIMPL>;
